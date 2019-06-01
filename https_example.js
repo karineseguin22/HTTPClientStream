e79eca36f0e42ba08377a0041634dba04f2cb013
karineseguin22 <karine.seguin2@mail.mcgill.ca>
2019-06-01 20:07:41 +0000

    added make a simple request

--- https_example.js
+++ https_example.js
@@ -1,3 +1,20 @@
 var https = require('https'); 
 
 console.log('I did it!'); 
+
+//make s simple HTTPS request to example.org 
+
+var options = {
+    host: 'www.example.org',
+    path: '/'
+};
+
+//called by https when the resquest is made
+var callback = function() {
+    console.log('In response handler callback!');
+}
+console.log("I'm about to make the request!"); 
+
+https.request(options, callback).end(); 
+
+console.log("I've made the request!"); 
